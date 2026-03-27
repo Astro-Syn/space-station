2736672030fedd554240d2c0171feb8199af92ef
Astro-Syn <kelseyamyb@gmail.com>
2026-03-27 11:14:26 -0400

    work on setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
+const APOD = () => <h2>Photo of the Day Page</h2>;
+const MarsRover = () => <h2>Mars Rover Page</h2>;
 
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}>
+                    <Route path="apod" element={<APOD />} />
+                    <Route path="marsRover" element={<MarsRover />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,20 @@
-import {Link} from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
     return (
         <div className='home-container'>
+            <div className='links-container'>
+                <Link to="/apod">Photo Of the Day</Link>
+                <Link to="marsRover">Mars Rover</Link>
+            </div>
 
             <div className='home-wrapper'>
                 <h1>Space Station</h1>
+
+               
+                <Outlet />
             </div>
-            
         </div>
     )
 }

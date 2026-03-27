f389cc8c731533af05ca2872238e5f2bb2a9cfbc
Astro-Syn <kelseyamyb@gmail.com>
2026-03-27 10:38:38 -0400

    add files

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+
+            <div className='home-wrapper'>
+                <h1>Space Station</h1>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home;
